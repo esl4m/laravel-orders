4fea0c78dbff36a79087c9d24ac549b27b8ae42a
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-10-31 00:47:47 +0200

    Adds users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's clear the users table first
+        // DB::table('users')->delete();
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('test');
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
