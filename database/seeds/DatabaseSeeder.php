d76d24cf479219a6a4f64510c6e5d6044c3f0d7a
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-10-31 00:48:46 +0200

    Adds seeders in database seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(ItemsTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
     }
 }
